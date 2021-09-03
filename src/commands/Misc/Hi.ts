fb061f79aa6a3487602d033c5a164dcb560c117c
Hiroto77 <83062954+Hiroto77@users.noreply.github.com>
2021-09-04 00:01:03 +0530

    Update Hi.ts

--- src/commands/Misc/Hi.ts
+++ src/commands/Misc/Hi.ts
@@ -15,11 +15,6 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (
-            await await request.buffer('https://i.pinimg.com/originals/2d/4e/bf/2d4ebf645d543791d1ca71d4177f4272.jpg'),
-                MessageType.image,
-                undefined,
-                undefined,
-            M.reply(`Hello! ${M.sender.username}!`))
+        return void (await M.reply(`Hi ${M.sender.username}!`))
     }
 }
