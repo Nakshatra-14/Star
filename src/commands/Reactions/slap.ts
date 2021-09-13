f2aa69517beda327a9a9a149207eb9b8259659b7
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-13 21:17:48 +0530

    Update slap.ts

--- src/commands/Reactions/slap.ts
+++ src/commands/Reactions/slap.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'slap',
             description: 'Slap someone',
-            category: 'Reactions',
+            category: 'reactions',
             usage: `${client.config.prefix}slap [tag/quote users]`,
         })
     }
