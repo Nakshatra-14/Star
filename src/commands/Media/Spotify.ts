5977c989d886fc9d344aae5b6c3bb2dd657b5bbd
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-13 21:00:56 +0530

    Update Spotify.ts

--- src/commands/Media/Spotify.ts
+++ src/commands/Media/Spotify.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
             description: 'Downloads given spotify track and sends it as Audio',
             category: 'media',
             usage: `${client.config.prefix}spotify [URL]`,
-            dm: true,
+            dm: false,
             baseXp: 20,
             aliases: ['sp']
         })
