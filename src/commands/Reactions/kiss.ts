8b38e9ca4890e89432a8617f01e6923f9a2f2d45
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-13 21:17:10 +0530

    Update kiss.ts

--- src/commands/Reactions/kiss.ts
+++ src/commands/Reactions/kiss.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'kiss',
             description: 'Kiss someone',
-            category: 'Reactions',
+            category: 'reactions',
             usage: `${client.config.prefix}kiss [tag/quote users]`,
         })
     }
