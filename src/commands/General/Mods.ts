754236fb764549dda4305af6ed357fae425c25ed
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-14 11:52:59 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -24,7 +24,7 @@ export default class Command extends BaseCommand {
                     user.notify || user.vname || user.name || 'null'
                 }*\n🍀 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`)
         )
-        text += `\nTo deploy your own Bot or To support Kaoi👾\nVisit : https://github.com/PrajjwalDatir/Kaoi `
+        text += `\nTo deploy your own Bot or To support Star ✨\nVisit : The URL of this Repo will soon be given after the modification `
         return void M.reply(text)
     }
 }
